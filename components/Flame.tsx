8ca910fc41cfdcf6941ab30b555591a575b06bdb
MG <53813521+meneergroot@users.noreply.github.com>
2026-01-04 19:31:52 +0100

    feat: Update Flame logo to new red fire design
    - Replace previous icon with new flame design matching provided image
    - Use coral red (#FF6B6B) for outer layer with darker outline
    - Use darker red (#E03B3B) for inner core with outline
    - Create teardrop-shaped flame with layered gradient effect

--- components/Flame.tsx
+++ components/Flame.tsx
@@ -12,17 +12,19 @@ export default function Flame({ className = "w-8 h-8" }: FlameProps) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
+      {/* Outer flame layer - bright coral red (#FF6B6B) with darker outline */}
       <path
-        d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.5-1.5-2.5-1.5-4C9.5 4.5 8 3 8 3S6.5 4.5 6.5 8c0 1.5-1.5 2.5-1.5 4a2.5 2.5 0 0 0 2.5 2.5Z"
-        fill="currentColor"
+        d="M12 2C10 4 8 6 7 8C6 10 5.5 12 6 14C6.5 16 8 17.5 10 18.5C12 19.5 14 19 15.5 17.5C17 16 18 14 18.5 12C19 10 18.5 8 17.5 6.5C16.5 5 15 4 13.5 3.5C12.5 3 12 2.5 12 2Z"
+        fill="#FF6B6B"
+        stroke="#FF6B6B"
+        strokeWidth="0.3"
       />
+      {/* Inner flame layer - darker red core (#E03B3B) with outline */}
       <path
-        d="M15.5 14.5A2.5 2.5 0 0 0 18 12c0-1.5-1.5-2.5-1.5-4C16.5 4.5 15 3 15 3s-1.5 1.5-1.5 5c0 1.5-1.5 2.5-1.5 4a2.5 2.5 0 0 0 2.5 2.5Z"
-        fill="currentColor"
-      />
-      <path
-        d="M12 21c-2.5 0-4.5-2-4.5-4.5 0-1 .5-2 1-3l3.5-6 3.5 6c.5 1 1 2 1 3 0 2.5-2 4.5-4.5 4.5Z"
-        fill="currentColor"
+        d="M12 4.5C10.8 5.8 10 7.2 9.5 8.5C9 9.8 9 11 9.2 12.2C9.4 13.4 10 14.4 11 15C12 15.6 13 15.5 13.8 14.8C14.6 14.1 15 13 15 12C15 11 14.6 9.8 13.8 9C13 8.2 12.2 7.8 12 4.5Z"
+        fill="#E03B3B"
+        stroke="#E03B3B"
+        strokeWidth="0.3"
       />
     </svg>
   );
