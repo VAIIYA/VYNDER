7c79a2a7a82979c09b1c128b035efbd4b2e68df1
MG <53813521+meneergroot@users.noreply.github.com>
2026-01-04 15:54:53 +0100

    fix: Remove extra closing div tag in profile page
    - Fix syntax error causing build failure
    - Remove duplicate closing div at line 670
    - Correct JSX structure for profile page grid layout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -665,10 +665,7 @@ export default function ProfilePage() {
                 </div>
               </div>
             </div>
-            </div>
           </div>
-          </div>
-        </div>
 
         {/* Action Buttons Section */}
         {editing && (
